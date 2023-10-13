6752fa29f68a14c1cd093400a97e554b98e31c3c
Florian Meissl <meissl.f@gmail.com>
2023-10-13 12:13:17 +0200

    Test all methods of RingBuffer

--- src/test/java/at/fhhagenberg/sqe/EmptyRingBufferTest.java
+++ src/test/java/at/fhhagenberg/sqe/EmptyRingBufferTest.java
@@ -3,7 +3,11 @@ package at.fhhagenberg.sqe;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class EmptyRingBufferTest {
+
     @Test
     public void testDequeue() {
         RingBuffer<Integer> ringBuffer = new RingBuffer<>(5);
@@ -11,5 +15,25 @@ public class EmptyRingBufferTest {
 
         Exception exc = assertThrows(RuntimeException.class, () -> ringBuffer.dequeue());
         assertEquals("Empty ring buffer.", exc.getMessage());
+    }
+
+    @Test
+    public void testPeek() {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(5);
+        assertTrue(ringBuffer.isEmpty());
+
+        Exception exc = assertThrows(RuntimeException.class, () -> ringBuffer.peek());
+        assertEquals("Empty ring buffer.", exc.getMessage());
+    }
+
+    @Test
+    public void testIteratorFullBuffer() {
+        int capacity = 5;
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(capacity);
+
+        Iterator<Integer> iter = ringBuffer.iterator();
+        for (int i = 0; i < capacity; i++) {
+            assertThrows(NoSuchElementException.class, () -> iter.next());
+        }
     }
 }
--- src/test/java/at/fhhagenberg/sqe/RingBufferTest.java
+++ src/test/java/at/fhhagenberg/sqe/RingBufferTest.java
@@ -3,6 +3,8 @@ package at.fhhagenberg.sqe;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+
 public class RingBufferTest {
     @Test
     public void testCapacity() {
@@ -14,7 +16,9 @@ public class RingBufferTest {
     public void testEnqueueElement() {
         RingBuffer<Integer> ringBuffer = new RingBuffer<>(5);
         ringBuffer.enqueue(2);
+        int elem = ringBuffer.peek();
 
+        assertEquals(2, elem);
         assertFalse(ringBuffer.isEmpty());
     }
 
@@ -28,5 +32,108 @@ public class RingBufferTest {
         int elem = ringBuffer.dequeue();
         assertEquals(2, elem);
         assertTrue(ringBuffer.isEmpty());
+    }
+
+    @Test
+    public void testEnqueueCapacityZero() {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(0);
+        assertThrows(IndexOutOfBoundsException.class, () -> ringBuffer.enqueue(1));
+    }
+
+    @Test
+    public void testEnqueueFullBufferDequeueEmptyBuffer() {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(3);
+
+        ringBuffer.enqueue(1);        
+        ringBuffer.enqueue(2);
+        ringBuffer.enqueue(3);
+
+        assertTrue(ringBuffer.isFull());
+        assertEquals(3, ringBuffer.size());
+
+
+        int elem = ringBuffer.dequeue();
+        assertEquals(1, elem);
+        assertEquals(2, ringBuffer.size());
+
+        elem = ringBuffer.dequeue();
+        assertEquals(2, elem);
+        assertEquals(1, ringBuffer.size());
+
+        elem = ringBuffer.dequeue();
+        assertEquals(3, elem);
+        assertEquals(0, ringBuffer.size());
+    }
+
+    @Test
+    public void testEnqueueFullBufferPlusOneDequeueEmptyBuffer() {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(3);
+
+        ringBuffer.enqueue(1);        
+        ringBuffer.enqueue(2);
+        ringBuffer.enqueue(3);
+
+        assertTrue(ringBuffer.isFull());
+        assertEquals(3, ringBuffer.size());
+
+        ringBuffer.enqueue(4);
+
+        assertTrue(ringBuffer.isFull());
+        assertEquals(3, ringBuffer.size());
+
+        int elem = ringBuffer.dequeue();
+        assertEquals(2, elem);
+        assertEquals(2, ringBuffer.size());
+
+        elem = ringBuffer.dequeue();
+        assertEquals(3, elem);
+        assertEquals(1, ringBuffer.size());
+
+        elem = ringBuffer.dequeue();
+        assertEquals(4, elem);
+        assertEquals(0, ringBuffer.size());
+    }
+
+    @Test
+    public void testIteratorFullBuffer() {
+        int capacity = 5;
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(capacity);
+
+        for (int i = 0; i < capacity; i++) {
+            ringBuffer.enqueue(i);
+        }
+
+        Iterator<Integer> iter = ringBuffer.iterator();
+        for (int i = 0; i < capacity; i++) {
+            assertEquals(i, iter.next());
+        }
+    }
+
+    @Test
+    public void testIteratorFullBufferPlusOne() {
+        int capacity = 5;
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(capacity);
+
+        for (int i = 0; i < capacity + 1; i++) {
+            ringBuffer.enqueue(i);
+        }
+
+        Iterator<Integer> iter = ringBuffer.iterator();
+        for (int i = 1; i < capacity + 1; i++) {
+            assertEquals(i, iter.next());
+        }
+    }
+
+    @Test
+    public void testIteratorRemove() {
+        int capacity = 5;
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(capacity);
+
+        for (int i = 0; i < capacity + 1; i++) {
+            ringBuffer.enqueue(i);
+        }
+
+        Iterator<Integer> iter = ringBuffer.iterator();
+        assertThrows(UnsupportedOperationException.class, () -> iter.remove());
     }
 }
